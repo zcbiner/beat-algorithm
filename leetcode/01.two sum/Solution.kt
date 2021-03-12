a630df9d103d59811eccceb49ddec95d4c9d5638
corbinzhong <corbinzhong@tencent.com>
2021-03-12 18:56:19 +0800

    优化第一题的解法

--- leetcode/01.two sum/Solution.kt
+++ leetcode/01.two sum/Solution.kt
@@ -1,12 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=kotlin
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = HashMap<Int, Int>()
+        val map = mutableMapOf<Int, Int>()
         for (i in 0..(nums.size - 1)) {
-            if (map.containsKey(nums[i])) {
-                return intArrayOf(map[nums[i]]!!, i)
+            map[nums[i]]?.let {
+                return intArrayOf(it, i)
             }
             map[target - nums[i]] = i
         }
-        return intArrayOf(-1, -1)
+        return intArrayOf()
     }
 }
+// @lc code=end
