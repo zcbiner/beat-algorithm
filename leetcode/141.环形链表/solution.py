8e58b75424ad2db7608e7e1225cff797e9ef6d1f
zcbin <405271667@qq.com>
2021-04-05 23:32:53 +0800

    提交题目： leetcode 141.环形链表

---
+++ leetcode/141.环形链表/solution.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=141 lang=python3
+#
+# [141] 环形链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        
+# @lc code=end
+
