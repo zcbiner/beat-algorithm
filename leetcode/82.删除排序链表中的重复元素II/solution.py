c20576302062b99fb4c88d76cdf9a0afe5b86000
zcbin <405271667@qq.com>
2021-04-05 23:13:10 +0800

    提交题目： leetcode 82.删除排序链表中的重复元素II

---
+++ leetcode/82.删除排序链表中的重复元素II/solution.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=82 lang=python3
+#
+# [82] 删除排序链表中的重复元素 II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+# @lc code=end
+
