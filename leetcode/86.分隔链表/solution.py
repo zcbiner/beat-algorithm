b9e8eb7ca2536b6e0876bd782481377cb1992966
zcbin <405271667@qq.com>
2021-04-05 23:18:23 +0800

    提交题目：leetcode 86.分隔链表

---
+++ leetcode/86.分隔链表/solution.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=86 lang=python3
+#
+# [86] 分隔链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+# @lc code=end
+
