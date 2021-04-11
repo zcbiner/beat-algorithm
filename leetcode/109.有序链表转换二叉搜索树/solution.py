09d3979dfefd75a05adb4ff0c1688384a06d770d
zcbin <405271667@qq.com>
2021-04-11 10:58:50 +0800

    提交题目：leetcode/109.有序链表转换二叉搜索树

---
+++ leetcode/109.有序链表转换二叉搜索树/solution.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=109 lang=python3
+#
+# [109] 有序链表转换二叉搜索树
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+# @lc code=end
+
