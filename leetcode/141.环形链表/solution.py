d1f3074874b77896f1830fc140e7f6c80bd635fd
zcbin <405271667@qq.com>
2021-04-11 12:22:30 +0800

    解题：leetcode/141.环形链表

--- leetcode/141.环形链表/solution.py
+++ leetcode/141.环形链表/solution.py
@@ -13,6 +13,14 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
+        p1 = head
+        p2 = head
+        while p1 and p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
+            if p1 == p2:
+                return True
+        return False
         
 # @lc code=end
 
