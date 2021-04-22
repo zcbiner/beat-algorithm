ec8a3ae6516bf84dde22906a2a421fca8946d7aa
zcbin <405271667@qq.com>
2021-04-22 08:38:50 +0800

    提交题目：leetcode/35.搜索插入位置

---
+++ leetcode/35.搜索插入位置/solution.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=35 lang=python3
+#
+# [35] 搜索插入位置
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+# @lc code=end
+
