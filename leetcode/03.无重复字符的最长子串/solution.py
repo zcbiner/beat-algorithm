65f8a2faa35cec677c55cde1bb5b8905a240ad46
zcbin <405271667@qq.com>
2021-04-24 23:09:29 +0800

    题目：LeetCode 03.无重复字符的最长子串

---
+++ leetcode/03.无重复字符的最长子串/solution.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLength = 1
+        map = {}
+        for i in range(len(s)):
+            if 
+# @lc code=end
+
