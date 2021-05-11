7d09a0a411078c52c6653cdf1000f76946a5381f
zcbiner <405271667@qq.com>
2021-05-11 20:53:11 +0800

    解题:leetcode/15.三数之和

---
+++ leetcode/15.三数之和/solution.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=15 lang=python3
+#
+# [15] 三数之和
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            # 排除重复数字
+            if i > 0 and nums[i] == nums[i -1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s > 0:
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return ans
+# @lc code=end
+
