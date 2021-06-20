78635c8a349858ef75ec8836d0e6fee70f70d488
zcbin <405271667@qq.com>
2021-06-20 23:21:17 +0800

    提交题目：leetcode/844.比较含退格的字符串

---
+++ leetcode/844.比较含退格的字符串/solution.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=844 lang=python3
+#
+# [844] 比较含退格的字符串
+#
+
+# @lc code=start
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+# @lc code=end
+
