35656f4f6e8cb69d7deec7808a13cb587308c907
zcbiner <405271667@qq.com>
2021-07-19 19:20:21 +0800

    解题:leetcode/70.爬楼梯

---
+++ leetcode/70.爬楼梯/solution.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        dp = [0,1,2]
+        for i in range(3, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[n]
+# @lc code=end
